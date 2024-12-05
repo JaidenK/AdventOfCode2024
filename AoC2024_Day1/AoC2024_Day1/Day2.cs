626fbd5dafed6cb2661be626bc6b3e1cd999565c
JaidenK <king.jaiden@yahoo.com>
2024-12-05 15:47:41 -0800

    Day 2 Part 1 Example input working

---
+++ AoC2024_Day1/AoC2024_Day1/Day2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024_Day1
+{
+    public class Day2
+    {
+        public readonly string[] ExampleInput =
+        {
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        };
+
+        List<List<int>> Reports;
+        public void ParseInputFile(string filename)
+        {
+            Reports = new List<List<int>>();
+            var lines = File.ReadAllLines(filename);
+            foreach (var line in lines)
+            {
+                Reports.Add(ParseLine(line));
+            }
+        }
+        public void ParseExampleInput()
+        {
+            Reports = new List<List<int>>();
+            foreach (var line in ExampleInput)
+            {
+                Reports.Add(ParseLine(line));
+            }
+        }
+
+        public string GetAnswerPart1()
+        {
+            if (Reports is null)
+                return "Reports NULL";
+            return Reports.Where(r => IsReportSafe(r)).Count().ToString();
+        }
+
+        public static List<int> ParseLine(string line)
+        {
+            var result = new List<int>();
+            foreach (var num in line.Split(' '))
+            {
+                result.Add(int.Parse(num));
+            }
+            return result;
+        }
+        public static bool IsReportSafe(List<int> report)
+        {
+            bool isSafe = true;
+            int direction = report[1] > report[0] ? 1 : -1;
+            for (int i = 1; i < report.Count; i++)
+            {
+                int difference = report[i] - report[i - 1];
+                difference *= direction;
+                isSafe &= (difference >= 1 && difference <= 3);
+            }
+            return isSafe;
+        }
+    }
+}
---
+++ AoC2024_Day1/UnitTestProject1/Day2Tests.cs
@@ -0,0 +1,29 @@
+﻿using AoC2024_Day1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Day2Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Day2.IsReportSafe(Day2.ParseLine("7 6 4 2 1")));
+            Assert.IsFalse(Day2.IsReportSafe(Day2.ParseLine("1 2 7 8 9")));
+            Assert.IsFalse(Day2.IsReportSafe(Day2.ParseLine("9 7 6 2 1")));
+            Assert.IsFalse(Day2.IsReportSafe(Day2.ParseLine("1 3 2 4 5")));
+            Assert.IsFalse(Day2.IsReportSafe(Day2.ParseLine("8 6 4 4 1")));
+            Assert.IsTrue(Day2.IsReportSafe(Day2.ParseLine("1 3 6 7 9")));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var d2 = new Day2();
+            d2.ParseExampleInput();
+            Assert.AreEqual("2",d2.GetAnswerPart1());
+        }
+    }
+}
