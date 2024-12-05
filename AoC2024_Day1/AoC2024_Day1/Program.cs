30b72095e3d83c8cd62a3ddc3eff4ca285a09edc
JaidenK <king.jaiden@yahoo.com>
2024-12-05 15:50:53 -0800

    Day 2 Part 1 complete

--- AoC2024_Day1/AoC2024_Day1/Program.cs
+++ AoC2024_Day1/AoC2024_Day1/Program.cs
@@ -10,10 +10,26 @@ namespace AoC2024_Day1
     {
         static void Main(string[] args)
         {
+            //Day1();
+            Day2();
+            Console.ReadKey();
+        }
+
+        static void Day1()
+        {
             var Day1 = new AoC_D1();
             //Console.WriteLine(Day1.GetAnswerPart1("input.txt"));
             Console.WriteLine(Day1.GetAnswerPart2("input.txt"));
-            Console.ReadKey();
+        }
+
+        static void Day2()
+        {
+            var Day2 = new Day2();
+
+            Console.WriteLine("Enter input filename:");
+            var filename = Console.ReadLine().Trim();
+            Day2.ParseInputFile(filename);
+            Console.WriteLine(Day2.GetAnswerPart1());
         }
     }
 }
